230de37227ca838680fcd92db3fc82919a68b63f
shafisahil <shafisahil7868@gamil.com>
2021-07-03 05:11:50 -0700

    some chnages

--- doubleClick.js
+++ doubleClick.js
@@ -1,5 +1,5 @@
 const loveMe = document.querySelector('.loveMe')
-const times = document.querySelector('#times')
+const times = document.querySelector('.times')
 
 let clickTime = 0
 let timesClicked = 0
@@ -10,6 +10,7 @@ loveMe.addEventListener('click', (e) =>{
         if((new Date().getTime()- clickTime) < 800){
             createHeart(e)
             clickTime = 0
+    
         }else{
             clickTime =new Date().getTime()
         }
@@ -17,19 +18,20 @@ loveMe.addEventListener('click', (e) =>{
 })
 
 const createHeart = (e) =>{
-    const heart = document.createElement('i')
+const heart = document.createElement('i')
     heart.classList.add('fas')
     heart.classList.add('fa-heart')
+   
     const x = e.clientX
     const y = e.clientY
 
-    const leftOffset = e.target.OffsetLeft
-    const topOffset = e.target.OffsetTop
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
 
     const xInside = x - leftOffset
-    const yInside = y - topOffset
+    const yInside = y - topOffset 
 
-    heart.style.top = `${yInside}px`
+    heart.style.top = `${yInside}px` 
     heart.style.left = `${xInside}px`
 
     loveMe.appendChild(heart)
@@ -37,4 +39,4 @@ const createHeart = (e) =>{
 
     setTimeout(() =>heart.remove(),1000)
         
-}
+ }
